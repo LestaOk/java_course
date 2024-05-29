d9f56592c1d4c0a07f33d067a8e225ad19907d44
Olga Kuzmina <olya-kuzm.kuzmina@yandex.ru>
2024-05-29 20:22:56 +0200

    Tasks 1-4

---
+++ src/main/java/ru/tink/Main.java
@@ -0,0 +1,10 @@
+package ru.tink;
+
+import java.util.logging.Logger;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger = Logger.getGlobal();
+        logger.info("Привет, мир!");
+    }
+}
